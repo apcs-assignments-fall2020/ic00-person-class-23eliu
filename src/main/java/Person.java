dde89aeb712d7ff2157bfb4a8479503be81ea818
23eliu <23eliu@athenian.org>
2020-10-26 19:19:48 +0000

    finished classwork

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,24 @@
 public class Person {
-    
+    //instance variables
+    public String firstName;
+    public String lastName;
+
+    //constructor
+    public Person(String a, String b) {
+        firstName = a;
+        lastName = b;
+    }
+
+    //method doSomething()
+    public void rishi() {
+        System.out.println("I am being obnoxious.");
+    }
+
+    //useful toString()
+    public String toString() {
+        String str = firstName + " " + lastName;
+        return str;
+    }
 }
 
 
--- src/main/java/PersonClient.java
+++ src/main/java/PersonClient.java
@@ -1,5 +1,7 @@
 public class PersonClient {
     public static void main(String[] args) {
-        
+        Person p1 = new Person("Rishi", "Reddy");
+        p1.rishi();
+        System.out.println(p1);
     }
 }
